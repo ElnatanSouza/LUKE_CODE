ff5f9115e23018e037eef68c0634c66c7540883c
elnat <elnatan.souzas@gmail.com>
2020-12-10 18:17:43 -0300

    Frontend plataforma de cursos em React com tematica de Star Wars!

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//Dependencias e Bibliotecas
+import React from 'react'
+//Components
+import Home from './pages/Home'
+// Assets: Imagem e Css
+import './assets/styles/global.css'
+
+const App = () => {
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
+
+export default App
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import './styles.css'
+
+import separator from '../../assets/images/separator.svg'
+import yoda from '../../assets/images/characters/yoda.png'
+
+const Home = () => {
+    return (
+        <>
+            <div className="section-1">
+                <div className="container">
+                    <header className="header">
+                        <h2>Luke Code</h2>
+                        <nav>
+                            <button className="btn-1">
+                                FAZER LOGIN
+                            </button>
+                            <button className="btn-2">
+                                INSCREVA-SE
+                            </button>
+                        </nav>
+                    </header>
+                    <main>
+                        <section>
+                            <h1>Se torne um mestre <br /> da programação</h1>
+                            <img src={separator} alt="" />
+                            <p>
+                                Dê o próximo passo para o seu futuro, <br />
+                                que a força esteja com você!
+                            </p>
+                        </section>
+                        <section>
+                            <img src={yoda} alt="Yoda - Star Wars" title="Yoda - Star Wars"/>
+                        </section>
+                    </main>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
