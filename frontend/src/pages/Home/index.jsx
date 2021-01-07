f841a5cf254a62e6b2e2eb25eed20bc36eb22d94
elnat <elnatan.souzas@gmail.com>
2021-01-07 18:25:43 -0300

    Added Carrousel

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -11,13 +11,16 @@ import coursePHP from '../../assets/images/courses/php.jpg'
 import courseFlutter from '../../assets/images/courses/flutter.jpg'
 import CardCourse from '../../components/CardCourse'
 import CardCategory from '../../components/CardCategory'
+import markRight from '../../assets/images/mark-right.svg'
+import markLeft from '../../assets/images/mark-left.svg'
+import userReviewImage from '../../assets/images/users/user.png'
 
 const Home = () => {
     return (
         <>
-            <Animation />
+            {/* <Animation /> */}
             <div className="section-1">
-                <div className="container">
+                <div className="box">
                     <header className="header">
                         <h2>Luke Code</h2>
                         <nav>
@@ -49,7 +52,7 @@ const Home = () => {
             </div>
 
             <div className="section-2">
-                <div className="container">
+                <div className="box">
                     <h2>+3 Cursos completos</h2>
                     <p>
                         Na Luke code, você tem acesso a 3 cursos gratuitos, basta se cadastrar na plataforma.
@@ -71,7 +74,7 @@ const Home = () => {
                 </div>
             </div>
             <div className="section-3">
-                <div className="container">
+                <div className="box">
                     <h2>O que oferecemos para você</h2>
                     <section className="categories-container">
                         <CardCategory title="BACK-END" courses="22 cursos" color="#E31B1B" image="https://cdn3.iconfinder.com/data/icons/web-design-development-shady/48/backend_website-512.png" />
@@ -83,6 +86,37 @@ const Home = () => {
                     </section>
                 </div>
             </div>
+
+            <div className="section-4">
+                <div className="box">
+                    <h2>Depoimentos</h2>
+                    <div className="image-review">
+                        <img src={userReviewImage} alt="Joaquim"/>
+                    </div>
+
+                    <div className="review">
+                        <img src={markLeft} alt="" />
+                        <p>
+                            Professor extremamente didático e com amplos conhecimentos <br />
+                            e o curso tem conteúdo que permite exercitar o que é ensinado!
+                        </p>
+                        <img src={markRight} alt="" />
+                    </div>
+
+                    <div className="user-review">
+                        <h4>
+                            Joaquim
+                        </h4>
+                    </div>
+                </div>
+
+            </div>
+
+            <footer>
+                <div className="box">
+                    &copy; {new Date().getFullYear()}  -  Luke Code - Alguns direitos reservados.
+                </div>
+            </footer>
         </>
     )
 }
